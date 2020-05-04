e08a2c952bf7d25d162e9aa0518640c56fd834d1
MicheleJin <10483883@polimi.it>
2020-05-04 16:43:01 +0200

    fix(script results)

--- scripts/kMean_blocking_test.sh
+++ scripts/kMean_blocking_test.sh
@@ -9,21 +9,19 @@
 # 	--attributes_list citruname city
 
 python3 ../blocking_starter.py \
-	--dataset restaurant \
+	--dataset cora \
 	--attributes_list author title \
 	--cluster_method kMean \
-	--num_clusters 100 \
+	--num_clusters 10 \
 	--dimension_reduction tsne \
 	--num_components 2 \
-	--perplexity 40 \
+	--perplexity 50 \
 	--distance_algorithm cosine \
 	--embedding_type inferSent \
 	--model_type bilstm \
 	--rnn_dim 2048 \
-	--model_version 2 
-
-
-#| tee -a ./outputs/outputs$(date +'%Y_%m_%d').txt
+	--model_version 2 \
+	| tee -a ./outputs/outputs$(date +'%Y_%m_%d').txt
 
 
 # python3 ../blocking_starter.py \
