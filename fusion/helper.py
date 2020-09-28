7a9f1fb0e20b1c29d1bd5aa87bacb4891cb5d115
MicheleJin <10483883@polimi.it>
2020-09-29 00:36:18 +0200

    add(new helper functions)

--- fusion/helper.py
+++ fusion/helper.py
@@ -1,8 +1,10 @@
 import numpy as np
+from preprocessing_datasets.preprocessing_utilities import ValueUtils
 
-def load_by_index(table_group_by_isbn, isbn_list, true_authors, index):
-    print('ISBN: {0}'.format(isbn_list[index]))
-    print('true author: {0}'.format(true_authors[index]))
+def load_by_index(table_group_by_isbn, isbn_list, true_authors, index, verbose):
+    if (verbose > 0):
+        print('ISBN: {0}'.format(isbn_list[index]))
+        print('true author: {0}'.format(true_authors[index]))
     table_ISBN = table_group_by_isbn.get_group(isbn_list[index])
     return table_ISBN, table_ISBN['author'].tolist(), true_authors[index]
 
@@ -19,15 +21,29 @@ def get_author_candidate_by_block(list_authors, chosenBlock):
     unique, counts = np.unique(candidates, return_counts=True)
     return dict(zip(unique, counts))
 
-def get_author_candidates(list_authors, blocks, lengthNecessary):
-    print("lengthNecessary: {0}".format(lengthNecessary))
+def get_author_candidates(list_authors, blocks, lengthNecessary, verbose):
     possibleAuthor = []
+    discartedAuthor = []
+
     indexSortedByLength = getSortedKeyByLength(blocks)
     for index in indexSortedByLength:
         block = blocks[index]
         if len(block) >= lengthNecessary:
             possibleAuthor.append(get_author_candidate_by_block(list_authors, block))
         else:
-            break
+            discartedAuthor.append(get_author_candidate_by_block(list_authors, block))
+    if (verbose > 0):
+        print("Discarted candidate: {0}".format(discartedAuthor))
+        print("Possible candidate: {0}".format(possibleAuthor))
+        print("lengthNecessary: {0}".format(lengthNecessary))
     return possibleAuthor
-        
+
+def getFinalAuthors(listCandidates):
+    solution = []
+    for candidate in listCandidates:
+        solution.append(max(candidate, key=candidate.get))
+    return solution
+
+def filterGoldenTruth(authors):
+    authors = ValueUtils.split_values(authors)
+    return [ValueUtils.clean_value(x) for x in authors]
